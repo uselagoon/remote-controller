61e18d2c6724a4c3123d6254120612b8080d48c0
Ben Jackson <b@benjackson.email>
2022-02-04 10:02:53 +1100

    fix: update apis in test resources

--- controller-test.sh
+++ controller-test.sh
@@ -219,9 +219,9 @@ build_deploy_controller
 echo "==> Trigger a lagoon build using kubectl apply"
 kubectl -n $CONTROLLER_NAMESPACE apply -f test-resources/example-project1.yaml
 # patch the resource with the controller namespace
-kubectl -n $CONTROLLER_NAMESPACE patch lagoonbuilds lagoon-build-7m5zypx --type=merge --patch '{"metadata":{"labels":{"lagoon.sh/controller":"'$CONTROLLER_NAMESPACE'"}}}'
+kubectl -n $CONTROLLER_NAMESPACE patch lagoonbuilds.v1beta1.lagoon.sh lagoon-build-7m5zypx --type=merge --patch '{"metadata":{"labels":{"lagoon.sh/controller":"'$CONTROLLER_NAMESPACE'"}}}'
 # patch the resource with a random label to bump the controller event filter
-kubectl -n $CONTROLLER_NAMESPACE patch lagoonbuilds lagoon-build-7m5zypx --type=merge --patch '{"metadata":{"labels":{"bump":"bump"}}}'
+kubectl -n $CONTROLLER_NAMESPACE patch lagoonbuilds.v1beta1.lagoon.sh lagoon-build-7m5zypx --type=merge --patch '{"metadata":{"labels":{"bump":"bump"}}}'
 sleep 10
 check_lagoon_build ${LBUILD}
 

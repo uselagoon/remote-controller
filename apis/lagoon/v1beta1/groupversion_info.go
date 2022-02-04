e2efb0ba9a898726617c34601c06e42b8cd0bef2
Ben Jackson <b@benjackson.email>
2022-02-04 18:20:01 +1100

    test: use crd.lagoon.sh

--- apis/lagoon/v1beta1/groupversion_info.go
+++ apis/lagoon/v1beta1/groupversion_info.go
@@ -15,7 +15,7 @@ limitations under the License.
 
 // Package v1beta1 contains API Schema definitions for the lagoon v1beta1 API group
 // +kubebuilder:object:generate=true
-// +groupName=lagoon.sh
+// +groupName=crd.lagoon.sh
 package v1beta1
 
 import (
@@ -25,7 +25,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "lagoon.sh", Version: "v1beta1"}
+	GroupVersion = schema.GroupVersion{Group: "crd.lagoon.sh", Version: "v1beta1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}

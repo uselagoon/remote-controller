2aff3a2d1228acb9d993b98c6699f331b18553c5
Ben Jackson <b@benjackson.email>
2022-05-02 14:07:36 +1000

    chore: remove some logging

--- controllers/v1beta1/lagoon_harbor22x.go
+++ controllers/v1beta1/lagoon_harbor22x.go
@@ -186,7 +186,6 @@ func (h *Harbor) CreateOrRefreshRobotV2(ctx context.Context,
 				deleted = true
 				continue
 			}
-			h.Log.Info(fmt.Sprintf("disable delete %v / %v", robot.Disable, h.DeleteDisabled))
 			if robot.Disable && h.DeleteDisabled {
 				// if accounts are disabled, and deletion of disabled accounts is enabled
 				// then this will delete the account to get re-created
@@ -203,7 +202,6 @@ func (h *Harbor) CreateOrRefreshRobotV2(ctx context.Context,
 				deleted = true
 				continue
 			}
-			h.Log.Info(fmt.Sprintf("should rotate %v / %s / %v", h.shouldRotate(robot.CreationTime.String(), h.RotateInterval), robot.CreationTime.String(), h.RotateInterval))
 			if h.shouldRotate(robot.CreationTime.String(), h.RotateInterval) {
 				// this forces a rotation after a certain period, whether its expiring or already expired.
 				h.Log.Info(fmt.Sprintf("Harbor robot account %s  should rotate, deleting it", robot.Name))
@@ -219,7 +217,6 @@ func (h *Harbor) CreateOrRefreshRobotV2(ctx context.Context,
 				deleted = true
 				continue
 			}
-			h.Log.Info(fmt.Sprintf("expires soon %v / %v / %v", h.expiresSoon(robot.ExpiresAt, h.ExpiryInterval), robot.ExpiresAt, h.ExpiryInterval))
 			if h.expiresSoon(robot.ExpiresAt, h.ExpiryInterval) {
 				// if the account is about to expire, then refresh the credentials
 				h.Log.Info(fmt.Sprintf("Harbor robot account %s  expires soon, deleting it", robot.Name))

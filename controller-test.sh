45d8e97a71ac5390c64c3446ae624a52113ac66c
shreddedbacon <b@benjackson.email>
2022-07-21 18:13:54 +1000

    test: try disable tlsverify

--- controller-test.sh
+++ controller-test.sh
@@ -158,6 +158,8 @@ kubectl create namespace lagoon
 helm upgrade --install -n lagoon lagoon-remote lagoon-remote/lagoon-remote \
     --set dockerHost.registry=http://harbor.172.17.0.1.nip.io:32080 \
     --set dockerHost.storage.size=50Gi \
+    --set dockerHost.extraEnvs[0].name=DOCKER_TLS_VERIFY \
+    --set dockerHost.extraEnvs[0].value=1 \
     --set dioscuri.enabled=false \
     --set dbaas-operator.enabled=false
 CHECK_COUNTER=1

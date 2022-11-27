c6f1d5bab51a2a8c82b070d1035c2c004c7cd803
shreddedbacon <b@benjackson.email>
2022-11-28 09:26:37 +1100

    chore: add some additional notes

--- internal/messenger/tasks_restore.go
+++ internal/messenger/tasks_restore.go
@@ -27,6 +27,7 @@ func (m *Messenger) ResticRestore(namespace string, jobSpec *lagoonv1beta1.Lagoo
 		// just log the error then return
 		return nil
 	}
+	// check the version, if there is no version in the payload, assume it is k8up v2
 	if vers == "backup.appuio.ch/v1alpha1" {
 		return m.createv1alpha1Restore(opLog, namespace, jobSpec)
 	} else {
@@ -37,6 +38,9 @@ func (m *Messenger) ResticRestore(namespace string, jobSpec *lagoonv1beta1.Lagoo
 	}
 }
 
+// checkRestoreVersionFromCore checks the message payload from lagoon to see if the version is provided
+// we do this as older versions of lagoon sent the entire payload to be created in the remote, newer versions of lagoon
+// will only send the data. since restores haven't changed in k8up this works
 func checkRestoreVersionFromCore(resource []byte) (string, error) {
 	misc := make(map[string]interface{})
 	if err := json.Unmarshal(resource, &misc); err != nil {
@@ -49,6 +53,7 @@ func checkRestoreVersionFromCore(resource []byte) (string, error) {
 	return "", nil
 }
 
+// createv1alpha1Restore will create a restore task using the restores.backup.appuio.ch v1alpha1 api (k8up v1)
 func (m *Messenger) createv1alpha1Restore(opLog logr.Logger, namespace string, jobSpec *lagoonv1beta1.LagoonTaskSpec) error {
 	restorev1alpha1 := &k8upv1alpha1.Restore{}
 	if err := json.Unmarshal(jobSpec.Misc.MiscResource, restorev1alpha1); err != nil {
@@ -75,6 +80,7 @@ func (m *Messenger) createv1alpha1Restore(opLog logr.Logger, namespace string, j
 	return nil
 }
 
+// createv1Restore will create a restore task using the restores.k8up.io v1 api (k8up v2)
 func (m *Messenger) createv1Restore(opLog logr.Logger, namespace string, jobSpec *lagoonv1beta1.LagoonTaskSpec) error {
 	restorev1 := &k8upv1.Restore{}
 	if err := json.Unmarshal(jobSpec.Misc.MiscResource, restorev1); err != nil {

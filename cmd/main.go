b543e38150b156b19b3b6a6d1f4baff6b9194302
shreddedbacon <b@benjackson.email>
2023-12-18 07:47:00 +1100

    feat: support idling messages from core

--- cmd/main.go
+++ cmd/main.go
@@ -52,6 +52,7 @@ import (
 	k8upv1 "github.com/k8up-io/k8up/v2/api/v1"
 	lagoonv1beta2 "github.com/uselagoon/remote-controller/api/lagoon/v1beta2"
 	harborctrl "github.com/uselagoon/remote-controller/internal/controllers/harbor"
+	namespacectrl "github.com/uselagoon/remote-controller/internal/controllers/namespace"
 	lagoonv1beta2ctrl "github.com/uselagoon/remote-controller/internal/controllers/v1beta2"
 	"github.com/uselagoon/remote-controller/internal/messenger"
 	k8upv1alpha1 "github.com/vshn/k8up/api/v1alpha1"
@@ -946,6 +947,20 @@ func main() {
 	// pre-seed the queues with the current state of tasks
 	if err := lagoonv1beta2.SeedTaskStartup(tmpClient, scheme, controllerNamespace, tasksCache, tasksQueueCache); err != nil {
 		setupLog.Error(err, "unable to seed controller startup state")
+	}
+
+	setupLog.Info("starting namespace controller")
+	// start the namespace reconciler
+	if err = (&namespacectrl.NamespaceReconciler{
+		Client:           mgr.GetClient(),
+		Log:              ctrl.Log.WithName("namespace").WithName("Namespace"),
+		Scheme:           mgr.GetScheme(),
+		EnableMQ:         enableMQ,
+		Messaging:        messaging,
+		LagoonTargetName: lagoonTargetName,
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Namespace")
+		os.Exit(1)
 	}
 
 	setupLog.Info("starting build controller")

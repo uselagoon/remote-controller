4389d4416f47e7627fcc9878e45d5cb8dc615e9e
shreddedbacon <shreddedbacon@users.noreply.github.com>
2025-11-24 17:32:24 +1100

    feat: add deployment replica watcher

--- cmd/main.go
+++ cmd/main.go
@@ -51,6 +51,7 @@ import (
 	"github.com/hashicorp/golang-lru/v2/expirable"
 	k8upv1 "github.com/k8up-io/k8up/v2/api/v1"
 	lagoonv1beta2 "github.com/uselagoon/remote-controller/api/lagoon/v1beta2"
+	deploymentsctrl "github.com/uselagoon/remote-controller/internal/controllers/deployments"
 	harborctrl "github.com/uselagoon/remote-controller/internal/controllers/harbor"
 	namespacectrl "github.com/uselagoon/remote-controller/internal/controllers/namespace"
 	lagoonv1beta2ctrl "github.com/uselagoon/remote-controller/internal/controllers/v1beta2"
@@ -960,6 +961,20 @@ func main() {
 		LagoonTargetName: lagoonTargetName,
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "Namespace")
+		os.Exit(1)
+	}
+
+	setupLog.Info("starting deployment controller")
+	// start the namespace reconciler
+	if err = (&deploymentsctrl.DeploymentsReconciler{
+		Client:           mgr.GetClient(),
+		Log:              ctrl.Log.WithName("deployments").WithName("Deployments"),
+		Scheme:           mgr.GetScheme(),
+		EnableMQ:         enableMQ,
+		Messaging:        messaging,
+		LagoonTargetName: lagoonTargetName,
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Deployments")
 		os.Exit(1)
 	}
 

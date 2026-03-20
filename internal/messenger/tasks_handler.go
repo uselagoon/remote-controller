ba1277bc97b28f358c13793cc44bbdb075f359c7
cgoodwin90 <chris_2190@hotmail.com>
2026-03-20 14:35:00 +1100

    Includes the deploy token for projectClone

--- internal/messenger/tasks_handler.go
+++ internal/messenger/tasks_handler.go
@@ -62,6 +62,7 @@ func (m *Messenger) ActiveStandbySwitch(namespace string, jobSpec *lagoonv1beta2
 // ProjectClone handles running the project clone archive task in the source environment.
 func (m *Messenger) ProjectClone(namespace string, jobSpec *lagoonv1beta2.LagoonTaskSpec) error {
 	jobSpec.AdvancedTask.SSHKey = true
+	jobSpec.AdvancedTask.DeployerToken = true
 
 	clonePayload := &ProjectClonePayload{}
 	clonePayloadDecoded, err := base64.StdEncoding.DecodeString(jobSpec.AdvancedTask.JSONPayload)
